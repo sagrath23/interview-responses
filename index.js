f290ea6b89dafdb6405d95be70692c1f1c41b2ce
Steven Villegas <sagrath23@gmail.com>
2021-02-04 18:48:38 +0000

    valoracion PSL - Juan Pablo Casas - Node

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-//////// 1. Generalidades ////////
-
-// Sabes que es una REST API??? Conoces los niveles de madurez de una RESTful API?
-// es el mecanismo para obtener información
-
-// HATEOAS
-
-// Sabes que diferencia hay entre localStorage y sessionStorage?
-// session: por tab y por dominio
-// local: por dominio
-
-// Sabes que es CORS?
-// los permisos para llamar las API. 
-
-//////// 2. Javascript ////////
+//////// 1. Javascript ////////
 
 // cual es la salida de cada console.log
 class Color {
@@ -39,20 +25,25 @@ const color2 = colorFactory('red');
 color1.color = 'yellow';
 color2.color = 'purple';
 
-color1.print(); //yellow
-
-color2.print();//red
+//color1.print(); //yellow
+//color2.print(); //red
 
 // cual es la salida de este código 
 function a() {
   console.log("a");
 }
 var b; // undefined
-a(); // a
-b(); // b is not a function
+a();
+b();
 b = function() {
   console.log("b");
-}
+}// error
+
+// let, const y var
+const a = [];
+a.push('foo');
+const b = {};
+b.foo = '';
 
 // cual es la salida de este object.hi()??
 const foo = 'Ren';
@@ -65,28 +56,29 @@ let object = {
   }
 };
 
-object.hi(); //Hi, Batsy & Robin
-// hi, undefined undefined
+object.hi();// Hi, Batsy & Robin
+// Hi, undefined & undefined
 
 // cual es la salida de estos ciclos?
 const a = { foo: 'bar', baz: 'yell', res: 'bal'};
 const b = ['kool', 'botz', 'tezt'];
-
+// key - value
+// next = () => ({ done: true, value: 'x' })
 for(let some in a) {
-  console.log(some); // foo, baz, res
-}
+  console.log(some);
+}// foo, baz, res
 
 for(let some in b) {
-  console.log(some); //0,1,2
-}
-// next ()
+  console.log(some);//'kool', 'botz', 'tezt'
+}// 0, 1, 2
+
 for(let some of a) {
-  console.log(some); //bra,yell, bal
-} // error
+  console.log(some);//foo, baz, res
+}// a is not iterable
 
 for(let some of b) {
-  console.log(some); // kool, botz, tezt
-}
+  console.log(some);
+}// kool, botz, tezt
 
 // en que orden se ejecutan los console.log y que imprimen?
 var a = 6;
@@ -106,27 +98,33 @@ var a = 6;
   a = 'tezt';
 })();
 console.log(a);
-// 1,2,baz,6,4,3
+
+//1
+//2
+//baz
+//6
+//4
+//3
 
 // puedes completar el codigo para que fileReader tenga este comportamiento?
-const fileReader = new Promise((r) => r('some'));
+const fileReader = async ()=>{ return 'some'};
 
-fileReader.then((data) => console.log(data, 'content')); // some, content
-
-const result = await fileReader
-console.log(result, 'content')
+fileReader().then((data) => console.log(data, 'content')); // some, content
 
 // puedes completar la implementacion de la clase Foo?
 class Foo {
-  total = 0;
+  count = 0;
   constructor() {}
-  add(n){
-    total +=n;
+
+  add(numberToAdd){
+    count += numberToAdd;
     return this;
   }
+
   getTotal(){
-    return total;
+    console.log(count);
   }
+
 }
 
 const foo = new Foo();
@@ -136,142 +134,57 @@ foo.add(1).add(2).add(3).add(4).getTotal();// should return 10
 // Punto extra!
 // puedes escribir una funcion que tenga este comportamiento ?
 sum(3)(5) // 8
-function sum(a,b){
-  if(b) return a+b;
-  return (b)=> a+b;
-}
-
 sum(3,5) // 8
 
-
-
-//////// 3. NPM ////////
+//////// 2. NPM ////////
 
 // Conoces que es NPM? que comandos conoces? has escuchado de npm link? npm pack?
-// start, npm run, npm i, uninstall
+// Node package manager, 
+// npm install -g
+
 // sabes que diferencia hay entre 1.2.3, ~1.2.3 y ^1.2.3 al instalar dependencias?
-// 1.2.3 exacta
-// ~ 1.a.x x la version mas alta
-// ^ 1.2.x 
+// 1.2.3 -> exacta
+//// ~1.2.3 -> 1.2.x
+//// ^1.2.3 -> 1.x.x
+// ^1.2.3 -> 4.5.6 
 // dependencies y devDependencies???
-// dependencies: lo que necesita para funcionar
-// devDependencies: lo que necesitas para desarrollar
+// dev env -> instala ambas
+// prod -> dependencies
+
 // peer dependencies??
 // 
 
-//////// 4. HTML ////////
+//////// 3. Node.js ////////
 
-// qué entiendes por HTML semántico??
-// descripcion de los tags html que hace referencia a su uso.
-// form, header, table, footer, 
-// section, aside
-// conoces sobre accesibilidad en ambientes web??
-//  
-// qué es el DOM?? 
-// es el contenido del sitio.
+// Qué es Node.js??
+// es un entorno de desarrollo en el que se usa JS para escribir codigo.
 
-//////// 5. CSS ////////
-// flexbox: si
-// flex-grow? flex-direction
-// flex-grow: size de los elementos hijos
-// flex-direction: row o column
-// grid
+// Qué librerias nativas de Node.js has utilizado??
+// fs, 
 
-// preprocesadores
-// less, sass, 
-// postCSS
-// puedes describir como se comportan estos selectores CSS?
-.div {
-  color: cyan;
+// Streams
 
-  &:hover {
-    color: red
-  }
-}
+// Event emmiter
 
-//////// 6. React ////////
-// sabes que es el vitualDOM?
-// es la forma de React validar que cambio con respecto al DOM real de la app.
+// Event loop
+
+// Profiling
+
+// Debug
+
+// Cómo manejas variables de entorno y secretos para usarlos en la ejecución de tu programa Node.js?
+
+// Express.js
+// js
+// middleware
 // 
-// sabes que es el lifecycle de React? Puedes describirlo?
-// 
-// qué diferencia hay entre estos??
-class Component extends React.Component {
-  ...
 
-  render() {
-    return (<div>Hi</div>);
-  }
-}
+const foo = {}
 
-class Component extends React.PureComponent {
-  ...
-  componentShouldUpdate() {...}
-  componentDidMount() {...}  
-  render() {
-    return (<div>Hi</div>);
-  }
-}
-// stateful components - class components
+foo && foo.bar && foo.bar.baz ||
 
-const Component = () => {
-  return (<div>Hi</div>);
-} // stateless component - functional component
-// Hooks
-
-// que consideras que es un hook? con cuales has trabajado??
-// 
-// useState -> estadon dentro de comp. funcionales
-// useEffect -> componentShouldUpdate
-// useMemo
-// useDispatch
-// useContext
-// useRef
-
-// cuantas veces se imprime el valor de a?
-const Component = ({ a }) => {
-  useEffect(() => {
-    console.log(a);
-  });
-
-  return (<div>Hi</div>);
-};
-
-// Puedes describir que es un HOC?
-// connect(mapStateToProps, mapDispatchToProps)(Component);
-const Component = ({ ...mapStateToProps, ...mapDispatchToProps});
-
-@connect(mapDispatchToProps, mapDispatchToProps)
-Component
-//
-
-// Has trabajado con Context en React?
-// pasar info entre componentes nesteado en profundidad.
-
-// Que papel cumple el Provider al usar Context en React?
-
-// y el Consumer?
-
-// Sabes para que se sirven los refs en React? 
-
-// portals: 
-
-// suspense: 
-
-
-//////// 7. Redux ////////
-
-// Puedes describir el patron arquitectonico que sigue redux?
-// tienes un estado inicial, que vas alterando para reflejar el estado actual de la informacion
-// 
-// Puedes describir los conceptos base de redux?
-
-// Que usas para conectar redux con un componente React?
-
-// Sabes que es un middleware?
-
-
-//////// 8. Fundamentos ////////
+foo?.bar?.baz || uuu; //
+//////// 4. Fundamentos ////////
 
 // OOP?
 
@@ -284,7 +197,7 @@ Component
 // por qué es mejor componer objetos en lugar de herencia clasica?
 
 
-//////// 9. Principios de diseño ////////
+//////// 5. Principios de diseño ////////
 
 // SOLID
 
@@ -293,7 +206,7 @@ Component
 // Dependency Injection / IoC
 
 
-//////// 10. Unit testing ////////
+//////// 6. Unit testing ////////
 
 // Podrías definir qué es un Unit Test?? Has utilizado Unit Tests en tu trabajo??
 
@@ -303,3 +216,11 @@ Component
 
 // Por qué una prueba unitaria debe ejecutarse de forma aislada?
 
+
+//////// 7. Misc ////////
+
+// Git (repos, branching strategies, tools)
+
+// CI/CD (Steps, pipelines, tools)
+
+// NoSQL
