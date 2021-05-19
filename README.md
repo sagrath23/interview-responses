4c7345ee222da3b0ccb2823079f0dd6542f67dc4
Steven Augusto Villegas <sagrath23@gmail.com>
2021-05-19 13:30:08 +0000

    valoracion react - david londono

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 A small repository to keep candidate's responses to tech validation interview.
 
 ### hello there!
+
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //////// 1. Generalidades ////////
 
 // Sabes que es una REST API??? Conoces los niveles de madurez de una RESTful API?
+// es una conexion con el backend, es el que me provee los recursos para el front.
 
 // Sabes que diferencia hay entre localStorage y sessionStorage?
+// session guarda cookie en localStorage
+// local: comun al browser
 
 // Sabes que es CORS?
+// son delicados porque pueden romper la app. es de seguridad, porque no permite que el navegador ejecute algo. 
 
 //////// 2. Javascript ////////
 
@@ -33,54 +37,66 @@ const color2 = colorFactory('red');
 color1.color = 'yellow';
 color2.color = 'purple';
 
-color1.print();// yellow
-color2.print();// red
+color1.print(); // cyan
+color2.print(); // red
 
 // cual es la salida de este código 
-let b; //
-a(); // a
-function a() {
+a();
+const = acaNombre => {
   console.log("a");
 }
-console.log(b);
-b(); // b is not a function
-b = function() {
+b();
+var b = function() {
   console.log("b");
 }
-
+// 
+function a {
+  console.log('a');
+}
+var b; // unidefined
+a(); // a
+b();// b is not a function
+b = function() {
+  console.log('b');
+}
+// Retornaria b es decir un string
 // cual es la salida de este object.hi()??
 const foo = 'Ren';
-const bar = 'Stimpy';
+const bar = 'Stimpy'
 let object = {
   foo: 'Batsy',
   bar: 'Robin',
-  hi: function() {
+  hi: () => {
     console.log(`Hi, ${this.foo} & ${this.bar}`)
   }
 };
-
-object.hi(); // Hi, undefined undefined
-
+// la salida por consola puede ser: Hi, Ren & Stimpy 
+object.hi(); // Hi, undefined & undefined
+// this.foo this.bar
 // cual es la salida de estos ciclos?
 const a = { foo: 'bar', baz: 'yell', res: 'bal'};
 const b = ['kool', 'botz', 'tezt'];
 
 for(let some in a) {
   console.log(some);
-} // foo, baz, res
-
+}// foo, baz, res
+// salida por consola: bar, yell, bal 
 for(let some in b) {
   console.log(some);
-} // 0, 1, 2
-
-// next: () => ({ value: 'x', done: true })
-for(let some of a) {
+  b[some];
+}// 0, 1, 2
+// salida por consola: kool 
+for(let some of a) { 
   console.log(some);
-} // error
-
+}
+// a is not iterable
+// next: () => ({ value: 'X', done: true })
+// salida por consola: bar
 for(let some of b) {
  console.log(some); 
-}// kool, botz, tezt
+}
+// kool, botz, tezt
+// salida por consola: kool
 
 // en que orden se ejecutan los console.log y que imprimen?
 var a = 6;
@@ -88,57 +104,34 @@ var a = 6;
   const foo = true;
   let a = 2;
 
-  console.log(1);
+  console.log(1); // ok
   setTimeout(() => {
-    console.log(foo == 1 ? 3 : 'bar');
+    console.log(foo == 1 ? 3 : 'bar'); //ok
   }, 1000);
-  console.log(a);
+  console.log(a); //ok 
   setTimeout(() => {
-    console.log(4);
+    console.log(4); //ok
   }, 0);
-  console.log(foo === 1 ? 5 : 'baz');
+  console.log(foo === 1 ? 5 : 'baz'); //ok
   a = 'tezt';
 })();
-console.log(a);
+console.log(a); //ok
+// salida: 1, tezt, 2, 4, bazz, 3
 // 1, 2, baz, 6, 4, 3
 
 // que diferencia hay entre let, var y const
-// var: scope global & local
-// let: scope global, local y de bloque
-// const: no se puede modificar para valores primitivos
-
-const [{ some: { value } }, ...rest] = [{ some: { value: '5'} }, 6, 4, 3];
-// value = 5
-// rest [6,4,3]
+// let es asignable, es menos estricta que const
+// var es asignable, vive en el ámbito global
+// const no es asignable, vive solo en su {}
 
 // puedes completar el codigo para que fileReader tenga este comportamiento?
 const fileReader = void 0;
-
-const fileReader = new Promise((resolve, reject) => {
-  resolve('some');
-});
-
-const fileReader = Promise.resolve('some');
-
-Promise.allSettled([]);
 
 fileReader.then((data) => console.log(data, 'content')); // some, content
 
 // puedes completar la implementacion de la clase Foo?
 class Foo {
-  constructor() {
-    this.counter = 0;
-  }
-
-  add(value) {
-    this.counter += value;
-
-    return this;
-  }
-
-  getTotal() {
-    return this.counter;
-  }
+  constructor() {}
 }
 
 const foo = new Foo();
@@ -154,8 +147,15 @@ sum(3,5) // 8
 //////// 3. NPM ////////
 
 // Conoces que es NPM? que comandos conoces? has escuchado de npm link? npm pack?
-
+// manejador de paquetes de node.
+// install
+// npx
+// 
 // sabes que diferencia hay entre 1.2.3, ~1.2.3 y ^1.2.3 al instalar dependencias?
+// latest -> 2.1.0
+// 1.2.3 -> 1.2.3
+// ~1.2.3 -> 1.2.X
+// ^1.2.3 -> 1.X.X
 
 // dependencies y devDependencies???
 
@@ -165,24 +165,20 @@ sum(3,5) // 8
 //////// 4. HTML ////////
 
 // qué entiendes por HTML semántico??
-// ciertos componentes de tu vista siguen una estructura recomendada: header-> section etc. que hacen mas facil la lectura del html para robots y humanos.
 
 // conoces sobre accesibilidad en ambientes web??
-// son caracteristicas que permiten a las personas con discapacidad interactuar con la web: zoom, colores, contraste
 
 // qué es el DOM?? 
-// es el arbol de nodos que se pueden manipular en tiempo de ejecucion.
 
 //////// 5. CSS ////////
 // Has trabajado con flexbox?
-// sirve para manejar las cajas 
+
 // sabes para qué sirve flex-grow? flex-direction?
-// flex-direction
-// flex-grow
+
 // Has utilizado grid?? sabes como definir áreas de renderizado??
-// 
+
 // preprocesadores
-// SaSS
+
 // puedes describir como se comportan estos selectores CSS?
 .div {
   color: cyan;
@@ -195,12 +191,11 @@ sum(3,5) // 8
 
 //////// 6. React ////////
 // sabes que es el vitualDOM?
-// es una representacion del DOM manejada en memoria, que tiene la capacidad de determinar en que puntos se han presentado cambios, que facilita la manipulacion del DOM. 
+// es una capa entre el dom y mi codigo para determinar que se tiene que visualizar
 
 // sabes que es el lifecycle de React? Puedes describirlo?
-// mounting -> componentDidMount
-// Update -> componentDidUpdate
-// unmounting -> componentWillUnmount
+// ciclo de vida del componente. 
+//  
 
 // qué diferencia hay entre estos??
 class Component extends React.Component {
@@ -209,85 +204,68 @@ class Component extends React.Component {
   render() {
     return (<div>Hi</div>);
   }
-} // class component - stateful components
+}// class component
 
 class Component extends React.PureComponent {
   ...
-  componentShouldUpdate() {
-
-  }
   render() {
     return (<div>Hi</div>);
   }
-}
+} // class component
 
 const Component = () => {
   return (<div>Hi</div>);
-} // functional component - stateless components
+} // 
 
 // Hooks
 
 // que consideras que es un hook? con cuales has trabajado??
-// es una feature de react que te permite interactuar con el lifecycle de react en componentes stateless.
-
-// useState
-// useContext
-// useRef
-// useEffect
 // 
 // useMemo
-// useCallback
-// useLayoutEffect
+// useState
+// useRef
+// useContext
+
+
 
 // cuantas veces se imprime el valor de a?
 const Component = ({ a }) => {
   useEffect(() => {
     console.log(a);
-  }, [a]);
+  });
 
   return (<div>Hi</div>);
-};// cada que cambie a // 
+};
 
 // Puedes describir que es un HOC?
-// es un copmponente que recibe un componente y retorna un nuevo componente.
+// comp. de orden superior
+// se encarga de compartir props a componentes. debe estar encima de los demas.
 
 // Has trabajado con Context en React?
-// sirve para compartir contextos (datos) entre componentes, para evitar pasarlos por props entre componentes.
-
-// Que papel cumple el Provider al usar Context en React?
-// es el que se encarga de definir que contexto va a estar disponible para los componentes hijos.
-
-// y el Consumer?
-// es el que extrae los valores del contexto proporcionado por un provider.
-
-// Sabes para que se sirven los refs en React? 
-// sirven para manipular el DOM. se crea una referencia al nodo del DOM. 
-
-// has trabajado con portals? 
 // 
 
-reactDom.render('#root')
-// has trabajado con suspense?? sabes para que sirve el fallback??
-// sirve para hacer lazy-loading, se puede defimir mientras carga se ponga un elemento diferente provisto en el fallback.
+// Que papel cumple el Provider al usar Context en React?
 
+// y el Consumer?
+
+// Sabes para que se sirven los refs en React?
+// toman referencias de un elemento, es como tener el "apellido del elemento para usarlo"
+
+// has trabajado con portals? 
+// para modales o notificaciones
+
+// has trabajado con suspense?? sabes para que sirve el fallback?? 
+// 
 //////// 7. Redux ////////
 
 // Puedes describir el patron arquitectonico que sigue redux?
-// flux, unidireccional, se tienen reducers donde se especifica la interaccion entre el state y las acciones.
 
 // Puedes describir los conceptos base de redux?
-// single state, el estado solo se actualiza a causa de una accion, inmutabilidad del estado (se genera un nuevo state por cada acccion), store unico para la app. 
 
 // Que usas para conectar redux con un componente React?
-// react-redux
-// connect(mapStateToProps, mapDispatchToProps)(Component)
-// useSelector, useDispatch
 
 // Sabes que es un middleware?
-// 
-// redux-thunks
-// redux-saga
-// redux-observable
+
 //////// 8. Fundamentos ////////
 
 // OOP?
@@ -313,6 +291,8 @@ reactDom.render('#root')
 //////// 10. Unit testing ////////
 
 // Podrías definir qué es un Unit Test?? Has utilizado Unit Tests en tu trabajo??
+
+// jest
 
 // Qué frameworks de Unit Tests conoces??
 
